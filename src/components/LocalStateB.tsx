2e950a369f63c48e74628022d79a23159220e5ca
take-tech1001 <tk.tech1001@gmail.com>
2021-10-28 22:58:39 +0900

    ➕ LocalSateB追加

---
+++ src/components/LocalStateB.tsx
@@ -0,0 +1,24 @@
+import type { VFC } from 'react'
+
+import { useState } from 'react'
+import { todoVar } from '../../cache'
+import { useReactiveVar } from '@apollo/client'
+import Link from 'next/link'
+
+const LocalStateB: VFC = () => {
+  const todos = useReactiveVar(todoVar)
+  return (
+    <>
+      {todos?.map((task, index) => {
+        return (
+          <p className="mb-3" key={index}>
+            {task.title}
+          </p>
+        )
+      })}
+      <Link href="/local-state-a">
+        <a>Back</a>
+      </Link>
+    </>
+  )
+}
