930d85afd086df06445c60bc509c68c85737e0d0
take-tech1001 <tk.tech1001@gmail.com>
2021-10-28 23:17:06 +0900

    ➕ local-state-aページ追加

---
+++ src/pages/local-state-a/index.page.tsx
@@ -0,0 +1,12 @@
+import { VFC } from 'react'
+import { LocalStateA } from '../../components/LocalStateA'
+import { Layout } from '../layout'
+
+const LocalStatePageA: VFC = () => {
+  return (
+    <Layout title="Local State A">
+      <LocalStateA />
+    </Layout>
+  )
+}
+export default LocalStatePageA
