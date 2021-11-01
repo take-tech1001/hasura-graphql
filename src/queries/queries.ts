e17fd6cf9176e604dad13deb3292e8376cfd689e
take-tech1001 <tk.tech1001@gmail.com>
2021-11-01 23:23:04 +0900

    🐛 クエリ修正

--- src/queries/queries.ts
+++ src/queries/queries.ts
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client'
 
 export const GET_USERS = gql`
   query GetUsers {
-    users(order_by: {created_at: desc}) {
+    users(order_by: { created_at: desc }) {
       id
       name
       created_at
@@ -11,26 +11,28 @@ export const GET_USERS = gql`
 `
 
 /*
-* @clientをつけることでgraphqlサーバーではなくクライアントのキャッシュを見に行く
-*/
+ * @clientをつけることでgraphqlサーバーではなくクライアントのキャッシュを見に行く
+ */
 export const GET_USERS_LOCAL = gql`
   query GetUsers {
-    users(order_by: {created_at: desc}) @client {
+    users(order_by: { created_at: desc }) @client {
       id
       name
       created_at
     }
   }
 `
-
 export const GET_USERIDS = gql`
   query GetUserIds {
-    users(order_by: {created_at: desc}) {
+    users(order_by: { created_at: desc }) {
       id
     }
   }
 `
 
+/*
+ * !の引数は必須
+ */
 export const GET_USERBY_ID = gql`
   query GetUserById($id: uuid!) {
     users_by_pk(id: $id) {
@@ -40,30 +42,24 @@ export const GET_USERBY_ID = gql`
     }
   }
 `
-
-/*
-* !の引数は必須
-*/
 export const CREATE_USER = gql`
   mutation CreateUser($name: String!) {
-    insert_users_one(object: {name: ""}) {
+    insert_users_one(object: { name: $name }) {
       id
       name
       created_at
     }
   }
 `
-
 export const UPDATE_USER = gql`
   mutation UpdateUser($id: uuid!, $name: String!) {
-    update_users_by_pk(pk_columns: {id: $id}, _set: {name: $name}) {
+    update_users_by_pk(pk_columns: { id: $id }, _set: { name: $name }) {
       id
       name
       created_at
     }
   }
 `
-
 export const DELETE_USER = gql`
   mutation DeleteUser($id: uuid!) {
     delete_users_by_pk(id: $id) {
