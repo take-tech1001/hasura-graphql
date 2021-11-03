fda4ba57aa63f8ffa215f630f34c0117421feae1
take-tech1001 <tk.tech1001@gmail.com>
2021-11-03 12:23:01 +0900

    ♻️ 再レンダリング防止

--- src/components/UserItem.tsx
+++ src/components/UserItem.tsx
@@ -1,5 +1,7 @@
-import type { VFC, memo, Dispatch, SetStateAction } from 'react'
+import type { VFC, Dispatch, SetStateAction } from 'react'
 import type { Users, DeleteUserMutationFn } from '../types/generated/graphql'
+
+import { memo } from 'react'
 
 type Props = {
   user: {
@@ -14,37 +16,37 @@ type Props = {
   >
 }
 
-export const UserItem: VFC<Props> = ({
-  user,
-  delete_users_by_pk,
-  setEditedUser,
-}) => {
-  return (
-    <div className="my-1">
-      <span className="mr-2">{user.name}</span>
-      <span className="mr-2">{user.created_at}</span>
-      <button
-        className="mr-1 py-1 px-3 text-white bg-green-600 hover:bg-green-700 rounded-2xl focus:outline-none"
-        data-testid={`edit-${user.id}`}
-        onClick={() => {
-          setEditedUser(user)
-        }}
-      >
-        Edit
-      </button>
-      <button
-        className="py-1 px-3 text-white bg-pink-600 hover:bg-pink-700 rounded-2xl focus:outline-none"
-        data-testid={`delete-${user.id}`}
-        onClick={async () => {
-          await delete_users_by_pk({
-            variables: {
-              id: user.id,
-            },
-          })
-        }}
-      >
-        Delete
-      </button>
-    </div>
-  )
-}
+export const UserItem: VFC<Props> = memo(
+  ({ user, delete_users_by_pk, setEditedUser }) => {
+    console.log('rendered')
+
+    return (
+      <div className="my-1">
+        <span className="mr-2">{user.name}</span>
+        <span className="mr-2">{user.created_at}</span>
+        <button
+          className="mr-1 py-1 px-3 text-white bg-green-600 hover:bg-green-700 rounded-2xl focus:outline-none"
+          data-testid={`edit-${user.id}`}
+          onClick={() => {
+            setEditedUser(user)
+          }}
+        >
+          Edit
+        </button>
+        <button
+          className="py-1 px-3 text-white bg-pink-600 hover:bg-pink-700 rounded-2xl focus:outline-none"
+          data-testid={`delete-${user.id}`}
+          onClick={async () => {
+            await delete_users_by_pk({
+              variables: {
+                id: user.id,
+              },
+            })
+          }}
+        >
+          Delete
+        </button>
+      </div>
+    )
+  }
+)
