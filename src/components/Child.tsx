a74708048efd3fcfeca5a94dbdc6858766928d42
take-tech1001 <tk.tech1001@gmail.com>
2021-11-06 00:42:13 +0900

    ✨ memo化検証用コンポーネント追加

---
+++ src/components/Child.tsx
@@ -0,0 +1,22 @@
+import type { ChangeEvent, FormEvent, VFC } from 'react'
+import { memo } from 'react'
+
+type Props = {
+  printMsg: () => void
+  handleSubmit: (e: FormEvent<HTMLFormElement>) => Promise<void>
+}
+
+export const Child: VFC<Props> = memo(({ printMsg, handleSubmit }) => {
+  return (
+    <>
+      {console.log('Child rendered')}
+      <p>Child Component</p>
+      <button
+        className="my-3 py-1 px-3 text-white bg-indigo-600 hover:bg-indigo-700 rounded-2xl focus:outline-none"
+        onClick={printMsg}
+      >
+        click
+      </button>
+    </>
+  )
+})
